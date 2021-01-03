865913b518225a4efeef6134eb41e418445b8951
mrmizangitlab <mrmizanbd93@gmail.com>
2021-01-03 13:34:12 +0600

    login added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 // import logo from './assets/images/logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './assets/css/App.css';
+import Alert from 'react-bootstrap/Alert'
 
 function App() {
   return (
     <div className="App">
-      <h3>welcome to react</h3>
+    <Alert variant="success">
+    <Alert.Heading>Hey, nice to see you</Alert.Heading>
+    <p>
+      Aww yeah, you successfully read this important alert message. This example
+      text is going to run a bit longer so that you can see how spacing within an
+      alert works with this kind of content.
+    </p>
+    <hr />
+    <p className="mb-0">
+      Whenever you need to, be sure to use margin utilities to keep things nice
+      and tidy.
+    </p>
+  </Alert>
     </div>
   );
 }
